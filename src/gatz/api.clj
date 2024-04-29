0eb1a589868ce0e95706139dba3f89da7ccabfb3
Sebastian Bensusan <sbensu@gmail.com>
2024-04-29 14:13:01 +0800

    move discussion endpoints to their own ns

--- src/gatz/api.clj
+++ src/gatz/api.clj
@@ -1,11 +1,11 @@
 (ns gatz.api
   "All the operations but in an API"
   (:require [chime.core :as chime]
-            [com.biffweb :as biff :refer [q]]
             [clojure.data.json :as json]
             [clojure.java.io :as io]
             [clojure.string :as str]
             [gatz.auth :as auth]
+            [gatz.api.discussion :as api.discussion]
             [gatz.api.message :as api.message]
             [gatz.connections :as conns]
             [gatz.db :as db]
@@ -26,11 +26,11 @@
    :headers {"Content-Type" "application/json"}
    :body (json/write-str body)})
 
-;; ======================================================================
-;; User
-
 (defn err-resp [err-type err-msg]
   (json-response {:type "error" :error err-type :message err-msg}))
+
+;; ======================================================================
+;; User
 
 (defn get-me
   [{:keys [biff/db auth/user-id] :as _ctx}]
@@ -179,209 +179,6 @@
     (json-response {:username username :available (nil? existing-user)})))
 
 ;; ====================================================================== 
-;; Discussions 
-
-(defmacro when-authorized-for-message [[user-id m] & body]
-  `(cond
-     (nil? ~user-id) (err-resp "not_logged_in" "You are not logged in")
-     (nil? ~m) (err-resp "missing_message" "Message not found")
-     (= ~user-id (:message/user_id ~m)) (do ~@body)
-     :else (err-resp "unauthorized" "You are unauthorized")))
-
-(defmacro if-authorized-for-discussion [[user-id d] & body]
-  `(cond
-     (nil? ~user-id) (err-resp "not_logged_in" "You are not logged in")
-     (nil? ~d) (err-resp "discussion_not_found" "Discussion not found")
-     (contains? (:discussion/members ~d) ~user-id) (do ~@body)
-     :else (err-resp "not_in_discussion" "You are not in this discussion")))
-
-(defmacro when-authorized-for-discussions [[user-id ds] & body]
-  `(cond
-     (nil? ~user-id) (err-resp "not_logged_in" "You are not logged in")
-     (or (nil? ~ds) (empty? ~ds)) (err-resp "discussion_not_found" "Discussion not found")
-
-     (not (every? (fn [d#]
-                    (contains? (:discussion/members d#) ~user-id))
-                  ~ds))
-     (err-resp "not_in_discussion" "You are not in this discussion")
-
-     :else (do ~@body)))
-
-(defmacro if-admin-for-discussion [[user-id d] & body]
-  `(cond
-     (nil? ~user-id) (err-resp "not_logged_in" "You are not logged in")
-     (nil? ~d) (err-resp "discussion_not_found" "Discussion not found")
-     (= (:discussion/created_by ~d) ~user-id)
-     (do ~@body)
-     :else (err-resp "not_admin" "You are not an admin for this discussion")))
-
-(defn get-discussion
-  [{:keys [biff/db biff.xtdb/node params auth/user-id] :as _ctx}]
-  (let [latest-tx (xt/latest-completed-tx node)
-        tx-id (some-> (:latest_tx params) mt/-string->long)]
-    (if (= tx-id (::xt/tx-id latest-tx))
-      (json-response {:current true
-                      :latest_tx {:id (::xt/tx-id latest-tx)
-                                  :ts (::xt/tx-time latest-tx)}})
-      (let [did (mt/-string->uuid (:id params))
-            {:keys [discussion messages user_ids]} (db/discussion-by-id db did)]
-        (if-authorized-for-discussion
-         [user-id discussion]
-         (json-response {:current false
-                         :latest_tx {:id (::xt/tx-id latest-tx)
-                                     :ts (::xt/tx-time latest-tx)}
-                         :discussion discussion
-                         :users (map (partial db/user-by-id db) user_ids)
-                         :messages (mapv crdt.message/->value messages)}))))))
-
-(defn ^:deprecated
-
-  mark-seen!
-
-  "Only for older clients"
-
-  [{:keys [biff/db auth/user-id params] :as ctx}]
-
-  {:pre [(uuid? user-id)]}
-  (let [did (mt/-string->uuid (:did params))
-        d (db/d-by-id db did)]
-    (if-authorized-for-discussion
-     [user-id d]
-     (do
-       (db/mark-as-seen! ctx user-id [did] (java.util.Date.))
-       (json-response {:status "ok"})))))
-
-(defn mark-many-seen! [{:keys [biff/db auth/user-id params] :as ctx}]
-  {:pre [(uuid? user-id)]}
-  (let [dids (map mt/-string->uuid (:dids params))
-        ds (mapv (partial db/d-by-id db) dids)]
-    (when-authorized-for-discussions
-     [user-id ds]
-     (do
-       (db/mark-as-seen! ctx user-id dids (java.util.Date.))
-       (json-response {:status "ok"})))))
-
-(defn mark-message-seen! [{:keys [biff/db auth/user-id params] :as ctx}]
-  {:pre [(uuid? user-id)]}
-  (let [did (mt/-string->uuid (:did params))
-        mid (mt/-string->uuid (:mid params))
-        d (db/d-by-id db did)]
-    (if-authorized-for-discussion
-     [user-id d]
-     (let [new-d (db/mark-message-seen! ctx user-id did mid (java.util.Date.))]
-       (json-response {:discussion new-d})))))
-
-(defn archive! [{:keys [biff/db auth/user-id params] :as ctx}]
-  {:pre [(uuid? user-id)]}
-  (let [did (mt/-string->uuid (:did params))
-        d (db/d-by-id db did)]
-    (if-authorized-for-discussion
-     [user-id d]
-     (let [d (db/archive! ctx user-id did (java.util.Date.))
-           {:keys [messages user_ids]} (db/discussion-by-id db did)]
-       (json-response {:discussion d
-                       :users (map (partial db/user-by-id db) user_ids)
-                       :messages (mapv crdt.message/->value messages)})))))
-
-(defn subscribe-to-discussion!
-  [{:keys [biff/db auth/user-id params] :as ctx}]
-  {:pre [(uuid? user-id)]}
-  (let [did (mt/-string->uuid (:did params))
-        d (db/d-by-id db did)]
-    (if-authorized-for-discussion
-     [user-id d]
-     (let [d (db/subscribe! ctx user-id did (java.util.Date.))]
-       (json-response {:discussion d})))))
-
-(defn unsubscribe-to-discussion!
-  [{:keys [biff/db auth/user-id params] :as ctx}]
-  {:pre [(uuid? user-id)]}
-  (let [did (mt/-string->uuid (:did params))
-        d (db/d-by-id db did)]
-    (if-authorized-for-discussion
-     [user-id d]
-     (let [d (db/unsubscribe! ctx user-id did (java.util.Date.))]
-       (json-response {:discussion d})))))
-
-
-;; The cut-off for discussions is when they were created but the feed sorting is
-;; based on when they were updated. This will close problems when there is more activity
-;; and certain updates are not reflected in the latest feed because they are not the latest...
-
-;; Can I push the seen and updated criteria to the database?
-
-;; I'll punt on this and think a little harder about it later
-
-;; discrepancy in how this gets params
-(defn get-full-discussions
-  [{:keys [biff/db biff.xtdb/node auth/user-id params] :as _ctx}]
-  (let [latest-tx (xt/latest-completed-tx node)
-        tx-id (some-> (:latest_tx params) mt/-string->long)]
-    (if (= (::xt/tx-id latest-tx) tx-id)
-      (json-response {:current true
-                      :latest_tx {:id (::xt/tx-id latest-tx)
-                                  :ts (::xt/tx-time latest-tx)}})
-      (let [dis (if-let [older-than-ts (some->> (:last_did params)
-                                                mt/-string->uuid
-                                                (db/discussion-by-id db)
-                                                :discussion
-                                                :discussion/created_at)]
-                  (db/discussions-by-user-id-older-than db user-id older-than-ts)
-              ;; This second function might not be sorting according to what the user saw
-                  (db/discussions-by-user-id-up-to db user-id))
-            ds (map (partial db/discussion-by-id db) dis)
-            users (db/all-users db)]
-        (json-response {:discussions ds
-                        :users users
-                        :current false
-                        :latest_tx {:id (::xt/tx-id latest-tx)
-                                    :ts (::xt/tx-time latest-tx)}})))))
-
-;; TODO: validate all params at the API level, not the db level
-;; TODO: members are not validated as existing
-(defn create-discussion! [{:keys [params biff/db] :as ctx}]
-  (def -dctx ctx)
-  (if-let [post-text (:text params)]
-    (if-not (db/valid-post? post-text (:media_id params))
-      (err-resp "invalid_post" "Invalid post")
-      (let [{:keys [discussion message]} (db/create-discussion-with-message! ctx params)]
-        ;; TODO: change shape of response
-        (json-response
-         {:discussion discussion
-          :users (mapv (partial db/user-by-id db) (:discussion/members discussion))
-          :messages [(crdt.message/->value message)]})))
-    (err-resp "invalid_params" "Invalid params: missing post text")))
-
-(defn add-member! [{:keys [params auth/user-id biff/db] :as ctx}]
-  (let [did (mt/-string->uuid (:discussion_id params))
-        uid (mt/-string->uuid (:user_id params))]
-    (if (and (uuid? did) (uuid? did))
-      (let [d (db/d-by-id db did)]
-        (if-admin-for-discussion
-         [user-id d]
-         (let [d (db/add-member! ctx {:discussion/id did :user/id uid})]
-           (json-response {:discussion d}))))
-      {:status 400 :body "invalid params"})))
-
-(defn create-message! [{:keys [params biff/db auth/user-id] :as ctx}]
-  (let [{:keys [text id discussion_id]} params
-        mid (let [mid (some-> id mt/-string->uuid)]
-              (if (uuid? mid) mid (random-uuid)))
-        media-id (some-> (:media_id params) mt/-string->uuid)
-        reply-to (some-> (:reply_to params) mt/-string->uuid)
-        did (mt/-string->uuid discussion_id)
-        d (db/d-by-id db did)]
-    (if-authorized-for-discussion
-     [user-id d]
-     (let [msg (db/create-message! ctx {:did did
-                                        :mid mid
-                                        :text text
-                                        :reply_to reply-to
-                                        :media_id media-id})]
-       (json-response {:message (crdt.message/->value msg)})))))
-
-
-;; ====================================================================== 
 ;; Websocket
 
 (defn connection-response [user-id conn-id]
@@ -520,8 +317,6 @@
               (doseq [ws wss]
                 (jetty/send! ws (json/write-str msg))))))))))
 
-
-
 #_(defn on-new-subscription [{:keys [biff.xtdb/node] :as ctx} tx]
     (let [db-before (xt/db node {::xt/tx-id (dec (::xt/tx-id tx))})]
       (doseq [[op & args] (::xt/tx-ops tx)
@@ -657,20 +452,20 @@
                  ["/file/presign" {:post presigned-url!}]
                  ["/media" {:post create-media!}]
 
-                 ["/message" {:post create-message!}]
+                 ["/message" {:post api.discussion/create-message!}]
                  ["/message/delete" {:post api.message/delete-message!}]
                  ["/message/edit" {:post  api.message/edit-message!}]
                  ["/message/react" {:post api.message/react-to-message!}]
                  ["/message/undo-react" {:post api.message/undo-react-to-message!}]
 
-                 ["/discussions" {:get get-full-discussions
-                                  :post create-discussion!}]
-                 ["/discussion" {:get get-discussion}]
-                 ["/discussion/mark-many-seen" {:post mark-many-seen!}]
+                 ["/discussions" {:get  api.discussion/get-full-discussions
+                                  :post api.discussion/create-discussion!}]
+                 ["/discussion" {:get   api.discussion/get-discussion}]
+                 ["/discussion/mark-many-seen" {:post api.discussion/mark-many-seen!}]
                  ;; this route is deprecated should be removed when all clients
                  ;; are upgraded
-                 ["/discussion/mark-seen" {:post mark-seen!}]
-                 ["/discussion/mark-message-seen" {:post mark-message-seen!}]
-                 ["/discussion/archive" {:post archive!}]
-                 ["/discussion/subscribe" {:post subscribe-to-discussion!}]
-                 ["/discussion/unsubscribe" {:post unsubscribe-to-discussion!}]]]})
+                 ["/discussion/mark-seen" {:post  api.discussion/mark-seen!}]
+                 ["/discussion/mark-message-seen" {:post api.discussion/mark-message-seen!}]
+                 ["/discussion/archive" {:post api.discussion/archive!}]
+                 ["/discussion/subscribe" {:post api.discussion/subscribe-to-discussion!}]
+                 ["/discussion/unsubscribe" {:post api.discussion/unsubscribe-to-discussion!}]]]})
