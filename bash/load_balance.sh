08df9addd504004f634e90c67354ae95a0def123
Sebastian Bensusan <sbensu@gmail.com>
2024-05-10 17:49:11 +0900

    sleep between loading jars so that they can all find rocks native module

--- bash/load_balance.sh
+++ bash/load_balance.sh
@@ -10,9 +10,12 @@ cleanup() {
 trap cleanup EXIT
 
 # Start three servers on different ports, pointing to bash/nginx_load_balancer.conf
-NODE_ID=alice PORT=8081 bash bash/jar.sh &
+NODE_ID=alice PORT=8081 bash bash/jar.sh & 
+sleep 0.1
 NODE_ID=bob PORT=8082 bash bash/jar.sh &
+sleep 0.1
 NODE_ID=charlie PORT=8083 bash bash/jar.sh &
+sleep 0.1
 
 nginx -c ~/dev/gatz/server/bash/nginx_load_balancer.conf
 
