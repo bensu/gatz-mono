632f3c5937bd751a313aa467290f16edb7827c93
Sebastian Bensusan <sbensu@gmail.com>
2024-05-25 16:59:23 -0400

    integrate ulid with clojure and xtdb

--- src/crdt/ulid.clj
+++ src/crdt/ulid.clj
@@ -1,8 +1,23 @@
 (ns crdt.ulid
-  (:import [java.io Writer]
-           [com.github.f4b6a3.ulid Ulid UlidCreator]))
+  (:require [xtdb.codec])
+  (:import [com.github.f4b6a3.ulid Ulid UlidCreator]
+           [java.io Writer]
+           [java.nio.charset StandardCharsets]
+           [org.agrona MutableDirectBuffer]))
 
 ;; https://github.com/f4b6a3/ulid-creator?tab=readme-ov-file
+
+;; ==================================================================
+;; API
+
+(defn random ^Ulid []
+  (UlidCreator/getUlid))
+
+(defn monotonic ^Ulid []
+  (UlidCreator/getMonotonicUlid))
+
+;; ==================================================================
+;; Integrations
 
 (defn read-ulid ^Ulid [^String s]
   (Ulid/from s))
@@ -12,10 +27,10 @@
   (.write writer "#crdt/ulid ")
   (print-method (.toString ulid) writer))
 
-(defn random ^Ulid []
-  (UlidCreator/getUlid))
+(defmethod print-dup Ulid [^Ulid ulid ^java.io.Writer w]
+  (.write w (str "#=(crdt.ulid/read-ulid " (pr-str (.toString ulid)) ")")))
 
-(defn monotonic ^Ulid []
-  (UlidCreator/getMonotonicUlid))
-
-
+(extend-protocol xtdb.codec/IdToBuffer
+  Ulid
+  (id->buffer [^Ulid ulid ^MutableDirectBuffer to]
+    (xtdb.codec/id-function to (.getBytes (.toString ulid) StandardCharsets/UTF_8))))
