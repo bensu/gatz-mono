65d21920c91d6033aa30f486bdf241f71ae6dac2
Sebastian Bensusan <sbensu@gmail.com>
2024-05-31 18:35:32 -0400

    set up bb test with the right ns to test

--- src/gatz/test.clj
+++ src/gatz/test.clj
@@ -1,4 +1,5 @@
 (ns gatz.test
+  (:gen-class)
   (:require [clojure.test :as test]
             [crdt.core]
             [gatz.api-test]
@@ -13,5 +14,16 @@
             [gatz.notify-test]
             [gatz.system]))
 
-(defn -main []
-  (clojure.test/run-all-tests))
+(defn -main [& args]
+  (clojure.test/run-tests
+   'crdt.core
+   'gatz.api-test
+   'gatz.api.contacts-test
+   'gatz.api.discussion-test
+   'gatz.api.group-test
+   'gatz.db.contacts-test
+   'gatz.db.discussion-test
+   'gatz.db.group-test
+   'gatz.db.message-test
+   'gatz.db.user-test
+   'gatz.notify-test))
