a67959c6d2dfbc5fda76d687e21cc164ba7c1d26
Sebastian Bensusan <sbensu@gmail.com>
2025-01-06 19:03:25 -0500

    pagination in the API

--- src/gatz/api/search.clj
+++ src/gatz/api/search.clj
@@ -5,6 +5,7 @@
             [gatz.crdt.discussion :as crdt.discussion]
             [gatz.db.search :as db.search]
             [gatz.db :as db]
+            [gatz.db.discussion :as db.discussion]
             [gatz.db.group :as db.group]
             [gatz.db.user :as db.user]
             [sdk.posthog :as posthog]
@@ -15,14 +16,10 @@
    :headers {"Content-Type" "application/json"}
    :body (json/write-str body)})
 
-
-(defn parse-search-params [params]
-  (if-let [term (:term params)]
-    (if (string? term)
-      {:term (str/lower-case (str/trim term))}
-      {})
-    {}))
-
+(defn parse-search-params [{:keys [term last_did]}]
+  (cond-> {}
+    (string? term) (assoc :term (str/lower-case (str/trim term)))
+    (string? last_did) (assoc :last_did (parse-uuid last_did))))
 
 (defn search-term
 
@@ -30,31 +27,31 @@
 
   (posthog/capture! ctx "search.term")
 
-  (if-let [term (:term (parse-search-params params))]
-    (let [search-results (->>
-                          (db.search/dids-for-uid db user-id term)
-                          (sort-by :search/last_ts)
-                          (reverse)
-                          (take 20))
-          blocked-uids (:user/blocked_uids (crdt.user/->value user))
-          poster-blocked? (fn [{:keys [discussion]}]
-                            (contains? blocked-uids (:discussion/created_by discussion)))
+  (let [{:keys [term last_did]} (parse-search-params params)]
+    (if (string? term)
+      (let [older-than-ts (some->> last_did (db.discussion/by-id db) :discussion/created_at)
+            search-results (->> (db.search/dids-for-uid db user-id term {:older-than-ts older-than-ts})
+                                (sort-by :search/last_ts)
+                                (reverse)
+                                (take 20))
+            blocked-uids (:user/blocked_uids (crdt.user/->value user))
+            poster-blocked? (fn [{:keys [discussion]}]
+                              (contains? blocked-uids (:discussion/created_by discussion)))
 
-          ds (->> search-results
-                  (map (fn [{:keys [search/did]}]
-                         (db/discussion-by-id db did)))
-                  (remove poster-blocked?))
+            ds (->> search-results
+                    (map (fn [{:keys [search/did]}]
+                           (db/discussion-by-id db did)))
+                    (remove poster-blocked?))
 
           ;; What are the groups and users in those discussions?
-          groups (->> (keep (comp :discussion/group_id :discussion) ds)
-                      (distinct)
-                      (mapv (partial db.group/by-id db)))
-          users (->> (map :user_ids ds)
-                     (reduce set/union)
-                     (mapv (partial db.user/by-id db)))]
-      ;; TODO: send the results as well 
-      (json-response {:discussions (mapv crdt.discussion/->value ds)
-                      :users (mapv crdt.user/->value users)
-                      :groups groups}))
-    (json-response {:discussions [] :users [] :groups []})))
+            groups (->> (keep (comp :discussion/group_id :discussion) ds)
+                        (distinct)
+                        (mapv (partial db.group/by-id db)))
+            users (->> (map :user_ids ds)
+                       (reduce set/union)
+                       (mapv (partial db.user/by-id db)))]
+        (json-response {:discussions (mapv crdt.discussion/->value ds)
+                        :users (mapv crdt.user/->value users)
+                        :groups groups}))
+      (json-response {:discussions [] :users [] :groups []}))))
 
--- src/gatz/db/search.clj
+++ src/gatz/db/search.clj
@@ -15,29 +15,36 @@
     [:search/last_ts inst?]
     [:search/results [:vector search-result]]]])
 
-(defn dids-for-uid [db uid text-query]
-  {:pre [(string? text-query) (uuid? uid)]}
-  (->> (q db '{:find [did mid created-at]
-               :in [text-query uid]
-               :order-by [[created-at :desc]]
-               :where [[(text-search :message/text text-query) [[mid]]]
-                       [mid :xt/id]
-                       [mid :db/type :gatz/message]
-                       [mid :message/did did]
-                       [mid :message/created_at created-at]
-                       [mid :message/deleted_at nil]
-                       [did :db/type :gatz/discussion]
-                       [did :discussion/members uid]]}
-          text-query
-          uid)
-       (map (fn [[did mid created-at]]
-              {:search/did did
-               :search/mid mid
-               :search/ts created-at}))
-       (group-by :search/did)
-       (map (fn [[did results]]
-              {:search/did did
-               :search/last_ts (->> results (map :search/ts) sort last)
-               :search/results results}))
-       (sort-by (fn [{:search/keys [last_ts]}] last_ts))
-       vec))
+(defn dids-for-uid
+  ([db uid text-query]
+   (dids-for-uid db uid text-query {}))
+  ([db uid text-query {:keys [older-than-ts]}]
+   {:pre [(string? text-query) (uuid? uid)
+          (or (nil? older-than-ts) (inst? older-than-ts))]}
+   (->> (q db {:find '[did created-at mid]
+               :in '[text-query uid older-than-ts]
+               :order-by '[[created-at :desc]]
+               :limit 2
+               :where (cond-> '[[(text-search :message/text text-query) [[mid]]]
+                                [mid :xt/id]
+                                [mid :db/type :gatz/message]
+                                [mid :message/did did]
+                                [mid :message/deleted_at nil]
+                                [did :db/type :gatz/discussion]
+                                [did :discussion/members uid]
+                                [did :discussion/created_at created-at]]
+                        older-than-ts (conj '[(< created-at older-than-ts)]))}
+           text-query
+           uid
+           older-than-ts)
+        (map (fn [[did created-at mid]]
+               {:search/did did
+                :search/mid mid
+                :search/ts created-at}))
+        (group-by :search/did)
+        (map (fn [[did results]]
+               {:search/did did
+                :search/last_ts (->> results (map :search/ts) sort last)
+                :search/results results}))
+        (sort-by (fn [{:search/keys [last_ts]}] last_ts))
+        vec)))
