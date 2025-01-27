2c6a960115b5415fc9701193b97e6d83c35ee2fa
Sebastian Bensusan <sbensu@gmail.com>
2025-01-27 13:10:42 -0300

    sentry: don't send events in dev

--- src/sdk/sentry.clj
+++ src/sdk/sentry.clj
@@ -2,24 +2,31 @@
   (:require [sentry-clj.core :as sentry]
             [clojure.tools.logging :as log]))
 
+(defonce ^:private enabled? (atom false))
+
+(defn send-request-error! [^Throwable e request]
+  (when @enabled?
+    (log/info "Logging exception to Sentry" (.getMessage e))
+    (sentry/send-event {:message (.getMessage e)
+                        :throwable e
+                        :request {"url" (:uri request)
+                                  "method" (-> request :request-method name)
+                                  "headers" (update-keys (:headers request) name)
+                                  "query_string" (:query-string request)}})))
+
 (defn wrap-sentry [handler]
   (fn [request]
     (try
       (handler request)
-      (catch Exception e
-        (log/info "Logging exception to Sentry" (.getMessage e))
-        (sentry/send-event {:message (.getMessage e)
-                            :throwable e
-                            :request {"url" (:uri request)
-                                      "method" (-> request :request-method name)
-                                      "headers" (update-keys (:headers request) name)
-                                      "query_string" (:query-string request)}})
+      (catch Throwable e
+        (send-request-error! e request)
         (throw e)))))
 
-(defn use-sentry [{:keys [biff/secret] :as ctx}]
-  (let [dsn (secret :sentry/dsn)
-        _ (assert dsn "Sentry DSN is not set")
-        env (or (secret "env") "development")]
+(defn use-sentry [{:keys [biff/secret biff/env] :as ctx}]
+  (let [dsn (secret :sentry/dsn)]
+    (assert dsn "Sentry DSN is not set")
     (log/info "Initializing Sentry with environment:" env)
-    (sentry/init! dsn {:environment env})
+    (when (= :env/prod env)
+      (sentry/init! dsn {:environment env})
+      (reset! enabled? true))
     ctx))
