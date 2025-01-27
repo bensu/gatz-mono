b3725cba80c4dda8b21cb7b4e38cb4e35d45fd25
Sebastian Bensusan <sbensu@gmail.com>
2025-01-27 20:57:43 -0300

    sentry: log events from transactions

--- src/sdk/sentry.clj
+++ src/sdk/sentry.clj
@@ -4,15 +4,30 @@
 
 (defonce ^:private enabled? (atom false))
 
+(defn send-event-error! [^Throwable e event]
+  (if @enabled?
+    (let [{:evt/keys [type did mid]} event]
+      (log/info "Logging event exception to Sentry:" (.getMessage e))
+      (sentry/send-event {:message (.getMessage e)
+                          :throwable e
+                          :request {"other" {"type" type "did" did "mid" mid}}}))
+    (do
+      (log/info "Sentry is not enabled")
+      (log/error e))))
+
 (defn send-request-error! [^Throwable e request]
-  (when @enabled?
-    (log/info "Logging exception to Sentry" (.getMessage e))
-    (sentry/send-event {:message (.getMessage e)
-                        :throwable e
-                        :request {"url" (:uri request)
-                                  "method" (-> request :request-method name)
-                                  "headers" (update-keys (:headers request) name)
-                                  "query_string" (:query-string request)}})))
+  (if @enabled?
+    (do
+      (log/info "Logging request exception to Sentry:" (.getMessage e))
+      (sentry/send-event {:message (.getMessage e)
+                          :throwable e
+                          :request {"url" (:uri request)
+                                    "method" (-> request :request-method name)
+                                    "headers" (update-keys (:headers request) name)
+                                    "query_string" (:query-string request)}}))
+    (do
+      (log/info "Sentry is not enabled")
+      (log/error e))))
 
 (defn wrap-sentry [handler]
   (fn [request]
@@ -22,11 +37,12 @@
         (send-request-error! e request)
         (throw e)))))
 
-(defn use-sentry [{:keys [biff/secret biff/env] :as ctx}]
-  (let [dsn (secret :sentry/dsn)]
+(defn use-sentry [{:keys [biff/secret] :as ctx}]
+  (let [dsn (secret :sentry/dsn)
+        sentry-env (secret :sentry/environment)]
     (assert dsn "Sentry DSN is not set")
-    (log/info "Initializing Sentry with environment:" env)
-    (when (= :env/prod env)
-      (sentry/init! dsn {:environment env})
+    (log/info "Initializing Sentry with environment:" sentry-env)
+    (when (= sentry-env "production")
+      (sentry/init! dsn {:environment sentry-env})
       (reset! enabled? true))
     ctx))
