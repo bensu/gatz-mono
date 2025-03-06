18cd5c4478529b1712e9f4dec74ba491c6a0967a
Sebastian Bensusan <sbensu@gmail.com>
2025-03-06 16:36:16 -0500

    manifest: document blocked version

--- src/gatz/settings.clj
+++ src/gatz/settings.clj
@@ -8,5 +8,6 @@
 (def manifest
   {:app {:min_version min-app-version
          :upgrade_message nil ;; Put a string here if you want to override the upgrade message
+         :blocked_version nil ;; XXX: this will brick the app if the version is older than this value
          :install_links {:ios "https://testflight.apple.com/join/K5OnqYuP"
                          :android "https://play.google.com/apps/internaltest/4701234533605084026"}}})
