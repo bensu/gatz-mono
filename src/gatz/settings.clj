e291d049d84018bdafb288d05e7e430284f864cf
Sebastian Bensusan <sbensu@gmail.com>
2025-03-15 14:55:25 -0400

    move min version to v1.1.12

--- src/gatz/settings.clj
+++ src/gatz/settings.clj
@@ -3,11 +3,11 @@
 (def app-name "gatz")
 
 ;; This should be in-sync with the frontend
-(def min-app-version "1.0.0")
+(def min-app-version "1.1.12")
 
 (def manifest
   {:app {:min_version min-app-version
          :upgrade_message nil ;; Put a string here if you want to override the upgrade message
          :blocked_version nil ;; XXX: this will brick the app if the version is older than this value
-         :install_links {:ios "https://testflight.apple.com/join/K5OnqYuP"
+         :install_links {:ios "https://apps.apple.com/us/app/gatz-chat/id6476069960"
                          :android "https://play.google.com/apps/internaltest/4701234533605084026"}}})
