9a3174e1b3187783a773ae9fc9eb829e6f66fc92
Sebastian Bensusan <sbensu@gmail.com>
2025-08-10 16:50:59 -0400

    bump version to v1.1.32

--- app/app.config.js
+++ app/app.config.js
@@ -3,7 +3,7 @@ export default {
     owner: "sbensu",
     name: "gatz.chat",
     slug: "gatz",
-    version: "1.1.31",
+    version: "1.1.32",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "automatic",
@@ -27,7 +27,7 @@ export default {
       },
     },
     android: {
-      versionCode: 124,
+      versionCode: 125,
       adaptiveIcon: {
         foregroundImage: "./assets/google_play_icon.png",
         backgroundColor: "#3D5135",
