6239c9464e0dc79149d75d6aa8a129e9abd10002
Sebastian Bensusan <sbensu@gmail.com>
2025-08-11 21:38:05 -0400

    bump to v1.1.42, new arch mostly working

--- app/app.config.js
+++ app/app.config.js
@@ -4,7 +4,7 @@ export default {
     owner: "sbensu",
     name: "gatz.chat",
     slug: "gatz",
-    version: "1.1.41",
+    version: "1.1.42",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "automatic",
@@ -28,7 +28,7 @@ export default {
       },
     },
     android: {
-      versionCode: 134,
+      versionCode: 135,
       adaptiveIcon: {
         foregroundImage: "./assets/google_play_icon.png",
         backgroundColor: "#3D5135",
